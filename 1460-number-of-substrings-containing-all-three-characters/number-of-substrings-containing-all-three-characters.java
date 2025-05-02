0cc9bca8d12a486faa64b1af16f826566a9b4119
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2025-05-02 03:38:25 -0700

    Time: 37 ms (14.59%) | Memory: 45.1 MB (21.81%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -0,0 +1,33 @@
+
+
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int n = s.length();
+        int i = 0, j = 0;
+        int result = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        while (j < n) {
+            // Add current character to the map
+            char ch = s.charAt(j);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            // While window contains all 3 characters
+            while (map.size() == 3) {
+                result += (n - j); // all substrings starting at i and ending from j to end
+
+                // Shrink window from left
+                char leftChar = s.charAt(i);
+                map.put(leftChar, map.get(leftChar) - 1);
+                if (map.get(leftChar) == 0) {
+                    map.remove(leftChar);
+                }
+                i++;
+            }
+
+            j++; // expand window from the right
+        }
+
+        return result;
+    }
+}
