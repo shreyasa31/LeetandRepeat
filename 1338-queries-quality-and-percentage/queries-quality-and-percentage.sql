63368c2cf21c590954b5339e5ab5b73b392c3269
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2025-05-20 16:03:20 -0700

    Time: 393 ms (34.81%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -3,3 +3,4 @@ select query_name, round(sum(rating/position)/count(query_name),2) as quality,
 round(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 1.0 / COUNT(rating) * 100,2) as poor_query_percentage
 from Queries
 group by query_name
+/*u cant put select inside sum*/
