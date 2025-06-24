b50287c570b281f22df3367cdbbdfa308cc77faf
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2025-06-23 22:50:00 -0700

    Time: 48 ms (47.46%) | Memory: 117.6 MB (20.49%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,75 +1,79 @@
 class Node{
-    Node prev;
-    Node next;
-    int key;
+    Node next,prev;
     int value;
-    Node (int key, int val){
+    int key;
+    public Node(int key,int value){
         this.key=key;
-        value=val;
-        prev=null;next=null;
+        this.value=value;
+        next=null;
+        prev=null;
     }
 }
 
 
+
+
+
+
 class LRUCache {
 
-
-HashMap<Integer, Node> map;
-int size;
-Node head; Node tail;
+    Node tail,head;
+    int size; 
+    HashMap<Integer, Node> map;
     public LRUCache(int capacity) {
-        map=new HashMap<>();
-        head=new Node(0,0);
         tail=new Node(0,0);
+        head=new Node(0,0);
+        map=new HashMap<>();
         size=capacity;
         head.next=tail;
         tail.prev=head;
+
     }
     
     public int get(int key) {
         if(map.containsKey(key)){
-            remove(map.get(key));
-            addLast(map.get(key));
-            return map.get(key).value;
+          remove(map.get(key));
+          addLast(map.get(key));
+          return map.get(key).value;
         }
-        return -1;
+      
+        return  -1;
+
     }
     
     public void put(int key, int value) {
+
         if(map.containsKey(key)){
+     
             remove(map.get(key));
         }
 
         Node newNode=new Node(key,value);
         addLast(newNode);
-        map.put(key,newNode);
+        map.put(key, newNode);
+   
 
-        if(map.size()>size){
+        if(map.size()>size)
+        {
             map.remove(head.next.key);
             remove(head.next);
-           
         }
-
 
     }
 
     void remove(Node node){
-         
-          
-          node.prev.next=node.next;
-          node.next.prev=node.prev;
+        node.prev.next=node.next;
+        node.next.prev=node.prev;
 
     }
-    
+
     void addLast(Node node){
-      Node temp=tail.prev;
-      node.next=tail;
-      tail.prev=node;  
-      temp.next=node;
-      node.prev=temp;
-
+         Node dummy=tail.prev;
+         tail.prev=node;
+         node.next=tail;
+         node.prev=dummy;
+         dummy.next=node;
     }
-    
 }
 
 /**
