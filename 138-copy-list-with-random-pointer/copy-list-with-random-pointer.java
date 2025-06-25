f3fca9024fa69a199615564d102c08914a4afce4
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2025-06-25 15:43:03 -0700

    Time: 0 ms (100.00%) | Memory: 44.9 MB (20.05%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -16,35 +16,35 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
         Node curr=head;
-        //copying the node and making connections
-        while(curr!=null){
+        while(curr!=null){ //copying next pointers like zigzag fashion creating clones
             Node node=new Node(curr.val);
             Node temp=curr.next;
             curr.next=node;
             node.next=temp;
             curr=temp;
-             
         }
+        if(head==null) return null;
 
-        //connecting the random pointers
-
+        //copying random pointers
         curr=head;
         while(curr!=null){
-            curr.next.random=(curr.random!=null)?curr.random.next:null;
+            curr.next.random=(curr.random==null)?null: curr.random.next;
             curr=curr.next.next;
         }
+        
+        //remove all the connections so that we can separate the two copies
 
-        //Remove connection and put it back
-        if(head==null) return null;
         Node oldNode=head;
         Node newNode=oldNode.next;
         Node newHead=newNode;
         while(oldNode!=null){
             oldNode.next=newNode.next;
-            newNode.next=(newNode.next!=null)?newNode.next.next:null;
+            newNode.next=newNode.next==null?null:newNode.next.next;
             oldNode=oldNode.next;
             newNode=newNode.next;
         }
+
         return newHead;
+
     }
 }
