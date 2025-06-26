eca95e0727aaf293a8d2d3aaf02eaf29954bbaa6
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2025-06-26 02:20:12 -0700

    Time: 1 ms (99.83%) | Memory: 53 MB (59.72%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,15 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
+        //for constant space use boyer moore algorithnm helpful to find out majority like more than n/k 
+        int candidate1=nums[0]; int count=0;
+        for(int num:nums){
+           if(num==candidate1){
+                count++;
+           }else{
+            count--;
+           }
 
-        for(int num:map.keySet()){
-            if(map.get(num)>nums.length/2){
-                return num;
-            }
+
+           if(count==0){
+            candidate1=num;
+            count=1;
+           }
         }
-        return -1;
+        return candidate1;
     }
 }
