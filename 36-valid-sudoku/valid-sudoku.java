c4b29c408f8057074ac215d9fb1f041a80951e2b
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2025-06-26 23:39:30 -0700

    Time: 3 ms (54.24%) | Memory: 44.4 MB (68.06%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,16 +1,16 @@
-
-
-import javax.swing.border.Border;
-
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Character>[] row=new HashSet[board.length];
-        HashSet<Character>[] col=new HashSet[board.length];
+        //rows=[haset1:row1, hashset2:row2 ......]
+        //so hashset datatype
+
+        HashSet<Character>[] rows=new HashSet[board.length];
+        HashSet<Character>[] cols=new HashSet[board.length];
         HashSet<Character>[] box=new HashSet[board.length];
 
-        for(int i=0;i<board.length;i++){
-            row[i]=new HashSet<Character>();
-            col[i]=new HashSet<Character>();
+        for(int i=0;i<board.length;i++)
+        {
+            rows[i]=new HashSet<Character>();
+            cols[i]=new HashSet<Character>();
             box[i]=new HashSet<Character>();
         }
 
@@ -20,19 +20,18 @@ class Solution {
                     continue;
                 }
 
-                if(row[i].contains(board[i][j])){
-                    return false;
+                if(rows[i].contains(board[i][j])){
+                     return false;
                 }
-                row[i].add(board[i][j]);
+                rows[i].add(board[i][j]);
 
-                 if(col[j].contains(board[i][j])){
-                    return false;
+                if(cols[j].contains(board[i][j])){
+                     return false;
                 }
-                col[j].add(board[i][j]);
+                cols[j].add(board[i][j]);
 
-                int idx= (i/3)*3 + (j/3);
-
-                 if(box[idx].contains(board[i][j])){
+                int idx=(i/3)*3+(j/3);
+                if(box[idx].contains(board[i][j])){
                     return false;
                 }
                 box[idx].add(board[i][j]);
