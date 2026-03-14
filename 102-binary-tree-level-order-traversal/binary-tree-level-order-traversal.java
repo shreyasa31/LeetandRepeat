0ec364f585b9d8ae55cca4a80c73c785c8555e19
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-03-13 22:43:33 -0500

    Time: 1 ms (95.07%) | Memory: 46.8 MB (77.55%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,25 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
        Queue<TreeNode> queue=new LinkedList<>();
-       
-       queue.add(root);
-       List<List<Integer>> res=new ArrayList<>();
+       ArrayList<List<Integer>> res=new ArrayList<>();
        if(root==null) return res;
-       int levell=0;
+       queue.add(root);
        while(!queue.isEmpty()){
-         res.add(new ArrayList<Integer>());
-
         int level=queue.size();
-        for(int  i=0;i<level;i++){
-            TreeNode curr=queue.poll();
-            res.get(levell).add(curr.val);
-            if(curr.left!=null) queue.add(curr.left);
-            if(curr.right!=null) queue.add(curr.right);
-        }
-         levell++;
+        res.add(new ArrayList<>());
+           for(int i=0;i<level;i++)
+           { 
+              TreeNode curr=queue.poll();
+              res.get(res.size()-1).add(curr.val);
+              if(curr.left!=null) queue.add(curr.left);
+              if(curr.right!=null) queue.add(curr.right);
+                
+           }
        }
        return res;
-      
-
     }
 }
