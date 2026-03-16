abe184da896f973bfa413e85607b4c6874f38a7d
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-03-16 04:24:11 -0500

    Time: 0 ms (100.00%) | Memory: 44.6 MB (97.55%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -15,10 +15,13 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root==null) return false;
-        int rem=targetSum-root.val;
+       if(root==null) return false;
+      
+       int rem=targetSum-root.val;
         if(root.left==null && root.right==null) return rem==0;
-        
-        return hasPathSum(root.left, rem) || hasPathSum(root.right, rem);
+        return hasPathSum(root.left, rem) ||
+        hasPathSum(root.right, rem);
+       
+         
     }
 }
