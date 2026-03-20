2ab1202a79c076e7841f9d029c76667641629161
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-03-20 02:10:04 -0500

    Time: 8 ms (10.77%) | Memory: 51.6 MB (85.43%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,31 +1,32 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int[][] dir = new int[][] {
-                { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 }
-        };
-        int count = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    count++;
-                    queue.offer(new int[] { i, j });
-                    grid[i][j] = '0';
+        Queue<int[]> queue=new LinkedList<>();
 
-                    while (!queue.isEmpty()) {
+        int[][] dirs={{1,0},{-1,0},{0,-1},{0,1}};
+        int count=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+               if(grid[i][j]=='1'){
+                  count++;
+                  queue.add(new int[]{i,j});
+                  grid[i][j]='0';
 
-                        int[] k = queue.poll();
-                        
-                        for (int[] d : dir) {
-int nr = k[0] + d[0];
-                        int nc = k[1] + d[1];
-                            if (nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && grid[nr][nc] == '1') {
-                                grid[nr][nc] = '0';
-                                queue.offer(new int[] { nr, nc });
-                            }
+                  while(!queue.isEmpty()){
+                    int[] curr=queue.poll();
+                     
+                    for(int[] k:dirs){
+                        int nr=curr[0]+k[0];
+                        int nc=curr[1]+k[1];
+
+                        if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length &&grid[nr][nc]=='1'){
+                            grid[nr][nc]='0';
+                            queue.add(new int[]{nr,nc});
                         }
                     }
-                }
+                  }
+                  
+
+               }
             }
         }
         return count;
