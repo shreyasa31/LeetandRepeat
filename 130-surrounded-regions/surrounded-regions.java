36b07a99b32437855647ba19fc07605930b7e833
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-03-20 22:13:37 -0500

    Time: 5 ms (10.53%) | Memory: 47.6 MB (90.86%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,62 +1,73 @@
-import java.util.*;
-
 class Solution {
     public void solve(char[][] board) {
-        int m = board.length;
-        int n = board[0].length;
-
-        int[][] DIRS = new int[][]{
-            {1, 0}, {-1, 0}, {0, 1}, {0, -1}
-        };
-
-        Queue<int[]> q = new LinkedList<>();
-
-        // Step 1  enqueue all border O cells
-        for (int r = 0; r < m; r++) {
-            if (board[r][0] == 'O') {
-                q.offer(new int[]{r, 0});
-                board[r][0] = 'T'; // mark as safe temporarily
-            }
-            if (board[r][n - 1] == 'O') {
-                q.offer(new int[]{r, n - 1});
-                board[r][n - 1] = 'T';
-            }
+       int rows=board.length;
+       int cols=board[0].length;
+       int[][] dirs={{1,0},{-1,0},{0,-1},{0,1}};
+      // first mark all borders if its 0 with T 
+       for(int i=0;i<rows;i++){
+        if(board[i][0]=='O'){
+            board[i][0]='T';
         }
-        for (int c = 0; c < n; c++) {
-            if (board[0][c] == 'O') {
-                q.offer(new int[]{0, c});
-                board[0][c] = 'T';
-            }
-            if (board[m - 1][c] == 'O') {
-                q.offer(new int[]{m - 1, c});
-                board[m - 1][c] = 'T';
-            }
+       }
+
+       for(int i=0;i<rows;i++){
+        if(board[i][cols-1]=='O'){
+            board[i][cols-1]='T';
         }
+       }
 
-        // Step 2  BFS to mark all O cells connected to the border as T
-        while (!q.isEmpty()) {
-            int[] cell = q.poll();
-            int r = cell[0], c = cell[1];
 
-            for (int[] d : DIRS) {
-                int nr = r + d[0];
-                int nc = c + d[1];
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n && board[nr][nc] == 'O') {
-                    board[nr][nc] = 'T';
-                    q.offer(new int[]{nr, nc});
+        for(int j=0;j<cols;j++){
+        if(board[0][j]=='O'){
+            board[0][j]='T';
+        }
+       }
+
+       for(int j=0;j<cols;j++){
+        if(board[rows-1][j]=='O'){
+            board[rows-1][j]='T';
+        }
+       }
+Queue<int[]> queue=new LinkedList<>();
+      //make connected to border T as well so do bfs from border
+      for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            if(board[i][j]=='T'){
+                queue.add(new int[]{i,j});
+
+                while(!queue.isEmpty()){
+                 int[] curr=queue.poll();
+
+                 for(int[] k: dirs){
+                    int nr=k[0]+curr[0];
+                    int nc=k[1]+curr[1];
+
+                    if(nr>=0 && nc>=0 && nr<rows && nc<cols && board[nr][nc]=='O'){
+                        board[nr][nc]='T';
+                        queue.add(new int[]{nr,nc});
+                    }
+                 }
                 }
             }
         }
+      }
 
-        // Step 3  finalize the board
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (board[r][c] == 'O') {
-                    board[r][c] = 'X';  // surrounded region captured
-                } else if (board[r][c] == 'T') {
-                    board[r][c] = 'O';  // restore safe cells
-                }
+      //chnage T to O and wherever O is there then add X
+      for(int i= 0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+
+            if(board[i][j]=='O'){
+                board[i][j]='X';
+            }else  if(board[i][j]=='T'){
+                board[i][j]='O';
             }
+
+            
         }
+      }
+      
+      
+
+
     }
 }
