3c81983ee23e8ac7a94ac12daa9aac3b6afa557d
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-03-20 23:53:47 -0500

    Time: 8 ms (64.85%) | Memory: 49.9 MB (86.02%) - LeetSync

---
+++ 286-walls-and-gates/walls-and-gates.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void wallsAndGates(int[][] rooms) {
+        int rows=rooms.length;
+        int cols=rooms[0].length;
+        int min=Integer.MAX_VALUE;
+        int[][] dirs={{1,0},{-1,0},{0,-1},{0,1}};
+        Queue<int[]> queue=new LinkedList<>();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(rooms[i][j]==0){
+                    queue.add(new int[]{i,j,1});
+                }
+            }
+        }
+        while(!queue.isEmpty()){
+            int[] curr=queue.poll();
+            int r=curr[0];
+            int c=curr[1];
+            int d=curr[2];
+            
+            for(int[] k:dirs){
+                int nr=k[0]+r;
+                int nc=k[1]+c;
+                
+          
+                if(nr>=0 && nc>=0 && nr<rows && nc<cols && rooms[nr][nc]==2147483647){
+                 
+                 rooms[nr][nc]=d;
+                 queue.add(new int[]{nr,nc,d+1});
+                     
+                }
+            }
+            
+        }
+
+    }
+}
