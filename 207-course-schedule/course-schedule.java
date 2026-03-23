757ee6aa9926a73dac9418a816cb187e1bc6684e
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-03-23 02:44:04 -0500

    Time: 14 ms (7.92%) | Memory: 47.9 MB (5.70%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,39 +1,36 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
-        List<List<Integer>> adj = new ArrayList<>(numCourses);
-
-        for (int i = 0; i < numCourses; i++) {
-            adj.add(new ArrayList<>());
+       ArrayList<List<Integer>> list=new ArrayList<>();
+       int[] indegree=new int[numCourses];
+       int count=0;
+       for(int i=0;i<numCourses;i++){
+        list.add(new ArrayList<>());
+       }
+       for(int[] k:prerequisites){
+        int first=k[0];
+        int second=k[1];
+        indegree[first]++;
+        list.get(second).add(first);
+       }
+       System.out.println(Arrays.toString(indegree));
+       Queue<Integer> queue=new LinkedList<>();
+       for(int i=0;i<indegree.length;i++){
+        
+           if(indegree[i]==0){
+            queue.add(i);
+           }
+       }
+    System.out.println(list);
+       while(!queue.isEmpty()){
+        int curr=queue.poll();
+        count++;
+        for(int n: list.get(curr)){
+            indegree[n]--;
+            if(indegree[n]==0){
+            queue.add(n);
+           }
         }
-
-        for (int[] prerequisite : prerequisites) {
-            adj.get(prerequisite[1]).add(prerequisite[0]);
-            indegree[prerequisite[0]]++;
-        }
-
-        Queue<Integer> queue = new LinkedList<>();
-        // Push all the nodes with indegree zero in the queue.
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                queue.offer(i);
-            }
-        }
-
-        int nodesVisited = 0;
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            nodesVisited++;
-
-            for (int neighbor : adj.get(node)) {
-                // Delete the edge "node -> neighbor".
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    queue.offer(neighbor);
-                }
-            }
-        }
-
-        return nodesVisited == numCourses;
-    }
+       }
+       return count==numCourses;
+    } 
 }
