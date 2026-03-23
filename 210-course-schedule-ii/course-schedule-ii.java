d9a436f0ac95fcb3419bbac0bd64c273be6bb7cd
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-03-23 02:57:40 -0500

    Time: 5 ms (82.32%) | Memory: 46.6 MB (93.78%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,41 +1,39 @@
-import java.util.*;
-
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>(numCourses);
-        for (int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
-
-        int[] indegree = new int[numCourses];
-
-        // build graph
-        for (int[] p : prerequisites) {
-            int course = p[0];
-            int pre = p[1];
-            adj.get(pre).add(course);   // pre -> course
-            indegree[course]++;         // one more requirement for course
-        }
-
-        // queue of courses ready to take
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) q.offer(i);
-        }
-
-        int[] order = new int[numCourses];
-        int idx = 0;
-
-        // Kahn BFS
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            order[idx++] = cur;
-
-            for (int nxt : adj.get(cur)) {
-                if (--indegree[nxt] == 0) q.offer(nxt);
+        int[] res=new int[numCourses];
+        ArrayList<List<Integer>> list=new ArrayList<>();
+       int[] indegree=new int[numCourses];
+       int count=0;
+       for(int i=0;i<numCourses;i++){
+        list.add(new ArrayList<>());
+       }
+       for(int[] k:prerequisites){ 
+        int first=k[0];
+        int second=k[1];
+        indegree[first]++;
+        list.get(second).add(first);
+       }
+    
+       Queue<Integer> queue=new LinkedList<>();
+       for(int i=0;i<indegree.length;i++){ 
+           if(indegree[i]==0){
+            queue.add(i);
+           }
+       }
+       int i=0;
+       while(!queue.isEmpty()){
+        int curr=queue.poll();
+        count++;
+        res[i]=curr;
+        i++;
+        for(int n:list.get(curr)){
+            indegree[n]--;
+            if(indegree[n]==0){
+                queue.add(n);
             }
         }
-
-        // if we scheduled all courses return order else cycle detected
-        if (idx == numCourses) return order;
-        return new int[0];
+        if(count==numCourses) return res;
+       }
+return new int[]{};
     }
 }
