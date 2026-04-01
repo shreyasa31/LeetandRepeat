318d8edd5fb874d5e755c4ca5dcedfb02ab9e87a
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-04-01 04:31:57 -0500

    Time: 7 ms (52.13%) | Memory: 66.4 MB (51.14%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        int numberofcomponents=n;
+         if(connections.length < n-1) {
+            return -1;
+        }
+        DSU d=new DSU(n); //create an object of the class
+
+        for(int[] c:connections){
+            if(d.find(c[0]) != d.find(c[1])){
+
+                numberofcomponents--;
+                d.union(c[0],c[1]);
+            }
+        }
+        return numberofcomponents-1;
+
+    }
+}
+
+class DSU{ // tc and sc is 4 alpha
+    int[] parent;
+    int[] rank; //union by rank // int[] size union by size
+
+    public DSU(int size){
+        parent=new int[size];
+        rank=new int[size]; //size[]=new int[n];
+        for(int i=0;i<size;i++){
+           parent[i]=i;
+           //size[i]=1;
+        
+         }
+    }
+
+    public int find(int x){
+        if(x==parent[x]){
+            return x;
+        }
+
+       parent[x]= find(parent[x]); //path compression to get constant time parent
+       return parent[x];
+
+    }
+
+    public boolean union(int x, int y){
+        int pu=find(x);
+        int py=find(y);
+
+        if(pu==py) return false;
+
+        if(rank[pu]<rank[py]){//size[pu]<Size[py] then parent[pu]=py and size[py]+=size[pu];
+            parent[pu]=py;
+        }else if(rank[py]<rank[pu]){
+            parent[py]=pu; //else size[py]<Size[pu] then parent[py]=pu and size[pu]+=size[py];
+        }else{
+            parent[py]=pu;
+            rank[pu]++;
+        }
+        return false;
+    }
+}
