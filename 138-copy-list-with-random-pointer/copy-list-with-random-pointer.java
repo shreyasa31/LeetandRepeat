0b8c33004753777b9ebcd3ace8012bf1c0839649
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-06-19 05:34:57 -0500

    Time: 0 ms (100.00%) | Memory: 46.5 MB (51.01%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,32 +15,36 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        if(head==null) return null;
+        if(head==null ) return null;
         Node curr=head;
         while(curr!=null){
-            Node newNode=new Node(curr.val);
-            Node temp=curr.next;
-            curr.next=newNode;
-            newNode.next=temp;
-            curr=temp;
+           Node newNode=new Node(curr.val);
+           Node temp=curr.next;
+           curr.next=newNode;
+           newNode.next=temp;
+           curr=temp;
+
+        }
+        curr=head;
+     
+        while(curr!=null){
+            curr.next.random=curr.random==null?null:curr.random.next;
+            curr=curr.next.next;
+
         }
 
-        curr=head;
-        while(curr!=null){
-            curr.next.random=(curr.random==null)?null:curr.random.next;
-            curr=curr.next.next;
-        }
-        
-        Node oldHead=head;
-        Node newHead=oldHead.next;
-        Node curr1=newHead;
-        while(oldHead!=null){
- 
-            oldHead.next=curr1.next;
-            oldHead=oldHead.next;
-            curr1.next=(curr1.next==null)?null:oldHead.next;
+        Node curr1=head;
+        Node newHead=curr1.next;
+        Node curr3=newHead;
+        while(curr1!=null){
+            curr1.next=curr1.next.next;
             curr1=curr1.next;
+
+            curr3.next=curr3.next==null?null:curr3.next.next;
+            curr3=curr3.next;
+
         }
+
         return newHead;
     }
 }
