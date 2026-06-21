96d2fd3bbc51934e86c1454871c9ee8b3ef833db
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-06-20 23:15:26 -0500

    Time: 2 ms (92.76%) | Memory: 43.5 MB (26.10%) - LeetSync

--- 390-elimination-game/elimination-game.java
+++ 390-elimination-game/elimination-game.java
@@ -1,16 +1,16 @@
 class Solution {
     public int lastRemaining(int n) {
-        return turns(n,true);
+        return solve(n, true);
     }
 
-    int turns(int n, boolean isTurn){
-        if(n==1) return 1;
+    int solve(int n, boolean isTurn){
+       if(n==1) return 1;
 
-        if(isTurn || n%2!=0){
-            return 2* turns(n/2, !isTurn);
-        }else
-        {
-            return 2* turns(n/2, !isTurn)-1;
-        }
+
+       if(isTurn || n%2!=0){
+             return 2 * solve(n/2, !isTurn);
+       }else{
+            return 2 * solve(n/2, !isTurn)-1;
+       }
     }
 }
