1ed093248ec41b2daa203c71642331eb8f78050e
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-06-21 05:02:47 -0500

    Time: 1 ms (31.01%) | Memory: 45.7 MB (23.02%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -15,15 +15,16 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root==null) return true;
-        int left=maxDepth(root.left);
-        int right=maxDepth(root.right);
-        return Math.abs(left-right)<2 && isBalanced(root.left) && isBalanced(root.right);
+        return maxDepth(root)!=-1;
     }
     public int maxDepth(TreeNode root) {
         if(root==null) return 0;
         int left=maxDepth(root.left);
+        if(left==-1) return -1;
         int right=maxDepth(root.right);
+        if(right==-1) return -1;
+
+        if(Math.abs(left-right)>1) return -1;
         return Math.max(left, right)+1;
     }
 }
