68d607d33b4163f93dd548bdd588506017e28611
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-06-25 03:38:44 -0500

    Time: 1 ms (99.99%) | Memory: 45.6 MB (39.12%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -14,22 +14,24 @@
  * }
  */
 class Solution {
-    List<List<Integer>> res=new ArrayList<>();
+     ArrayList<Integer> s=new ArrayList<>();
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        ArrayList<Integer> s= new ArrayList<>();
-        return solve(root, 0, targetSum, s);
+        List<List<Integer>> res=new ArrayList<>();
+        ArrayList<Integer> s=new ArrayList<>();
+        return solve(root, res, s, 0, targetSum);
     }
-    List<List<Integer>> solve(TreeNode root, int sum, int targetSum, List<Integer> s){
-       if(root==null) return res;
+    List<List<Integer>> solve(TreeNode root, List<List<Integer>> res,List<Integer> s,int sum, int targetSum){
        
+        if(root==null ) return res;
         s.add(root.val);
         sum=sum+root.val;
-        if(root.left==null && root.right==null && sum==targetSum){
-             res.add(new ArrayList<>(s));
-             
+        if(root.left==null && root.right==null && sum==targetSum ){
+            res.add(new ArrayList<>(s));
+
         }
-        solve(root.left, sum, targetSum, s);
-        solve(root.right, sum, targetSum, s);
+       
+        solve(root.left, res,s,sum, targetSum);
+        solve(root.right, res,s, sum, targetSum);
         s.remove(s.size()-1);
         return res;
 
