2176f425847ecc7554163083bde868a4eb3c2e31
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-06-26 00:58:33 -0500

    Time: 0 ms (100.00%) | Memory: 47.8 MB (5.35%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -1,4 +1,5 @@
-/**
+
+import java.lang.module.FindException;/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -15,47 +16,54 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) return null;
-
-        if (root.val == key) {
+        if(root==null){
+           return null;
+        }
+        if(root.val==key){
             return helper(root);
         }
+        TreeNode dummy=root;
+        while(root!=null){
 
-        TreeNode cur = root;
 
-        while (cur != null) {
-            if (key < cur.val) {
-                if (cur.left != null && cur.left.val == key) {
-                    cur.left = helper(cur.left);
-                    break;
-                } else {
-                    cur = cur.left;
-                }
-            } else {
-                if (cur.right != null && cur.right.val == key) {
-                    cur.right = helper(cur.right);
-                    break;
-                } else {
-                    cur = cur.right;
-                }
-            }
-        }
-        return root;
+        if(root.val>key){
+              if(root.left!=null && root.left.val==key){
+                root.left=helper(root.left);
+                break;
+              }else{
+                root=root.left;
+              }
+        }else{
+             if(root.right!=null && root.right.val==key){
+                root.right=helper(root.right);
+                break;
+              }else{
+                root=root.right;
+              }
+        }}
+
+        return dummy;
+
+
     }
+    TreeNode helper(TreeNode root){
+         if(root.left==null)
+         {
+            return root.right;
+         }else if(root.right==null)
+         {
+            return root.left;
+         }else{
+            TreeNode rightChild=root.right;
+            TreeNode leftChildRight=findLastRight(root.left);
+            leftChildRight.right=rightChild;
+              return root.left;
 
-    // returns the subtree that should replace node after deletion
-    private TreeNode helper(TreeNode node) {
-        if (node.left == null) return node.right;
-        if (node.right == null) return node.left;
-
-        TreeNode rightChild = node.right;
-        TreeNode lastRight = findLastRight(node.left);
-        lastRight.right = rightChild;
-        return node.left;
+         }
+       
     }
-
-    private TreeNode findLastRight(TreeNode node) {
-        while (node.right != null) node = node.right;
-        return node;
+    TreeNode findLastRight(TreeNode root){
+            if(root.right==null) return root;
+            return findLastRight(root.right);
     }
 }
