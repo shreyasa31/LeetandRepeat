405b7638eb1257c9279530e28e7fc1cc73917d73
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-06-25 22:00:34 -0500

    Time: 2 ms (40.37%) | Memory: 44.9 MB (87.39%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -15,32 +15,41 @@
  */
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if(root==null) return 0;
-       Queue<Pair<TreeNode,Integer>> queue=new LinkedList<>();
-       queue.add(new Pair<>(root,0));
-       int max=Integer.MIN_VALUE;
-       int idx=0;
-       while(!queue.isEmpty())
-       {
-           int level=queue.size();
-           int min=Integer.MAX_VALUE;int f=0;int l=0;
-           for(int i=0;i<level;i++){
+        Queue<Pair<TreeNode, Integer>> queue=new LinkedList<>();
+        queue.add(new Pair<>(root, 0));
+        int idx=0; 
+        int max=Integer.MIN_VALUE;
+        int f=0;int l=0;
+        while(!queue.isEmpty()){
 
-         
-           Pair<TreeNode,Integer> curr=queue.poll();
-           TreeNode node=curr.getKey();
-           idx=curr.getValue();
-           if(i==0) f=idx;
-           if(i==level-1) l=idx;
-        
-           min=Math.min(min,idx);
-           int a=idx-min;
-           if(node.left!=null) queue.add(new Pair<>(node.left, 2*a+1 ));
-           if(node.right!=null) queue.add(new Pair<>(node.right, 2*a+2));
-           }
-           max=Math.max(max, l-f+1);
+            int level=queue.size();
+            int min=Integer.MAX_VALUE;
+            for(int i=0;i<level;i++){
+                Pair<TreeNode, Integer> p=queue.poll();
+                TreeNode key=p.getKey();
+                idx=p.getValue();
 
-       }
-       return max;
+                if(i==0) f=idx;
+                if(i==level-1) l=idx;
+
+                min=Math.min(min,idx);
+
+                int a= idx-min;
+                if(key.left!=null){
+                    queue.add(new Pair<>(key.left, 2*a+1));
+                }
+                if(key.right!=null){
+                    queue.add(new Pair<>(key.right, 2*a+2));
+                }
+
+                
+
+
+            }
+            max=Math.max(max, l-f+1);
+           
+
+        }
+        return max;
     }
 }
