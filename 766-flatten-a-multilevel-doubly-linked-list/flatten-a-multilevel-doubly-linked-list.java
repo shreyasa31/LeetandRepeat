caf754efc1a12a294e89555d700f003df3ae1f35
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-06-27 13:51:08 -0500

    Time: 3 ms (6.05%) | Memory: 43.5 MB (56.71%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
@@ -10,23 +10,30 @@ class Node {
 
 class Solution {
     public Node flatten(Node head) {
-        Node curr=head;
+        solve(head);
+        return head;
+    }
+    public Node solve(Node head) {
+        Node curr=head; Node tail=curr;
         while (curr!=null) {
             Node curr1=curr.next;
+            tail=curr;
             if(curr.child!=null){
              Node chld=curr.child;
+             Node childTail=solve(chld);
              curr.next=chld;
              chld.prev=curr;
              curr.child=null;
-             while(chld.next!=null){
-                chld=chld.next;
-             }
-             chld.next=curr1;
+            //  while(chld.next!=null){
+            //     chld=chld.next;
+            //  }
+             childTail.next=curr1;
              if(curr1!=null)
-             curr1.prev=chld;
+             curr1.prev=childTail;
+             tail=childTail;
             }
              curr=curr.next;
         }
-        return head;
+        return tail;
     }
 }
