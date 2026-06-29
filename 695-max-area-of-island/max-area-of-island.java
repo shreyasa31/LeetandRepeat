5e12528fe58dc1706e47cb0393855521e35c58d8
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-06-29 01:34:05 -0500

    Time: 13 ms (3.29%) | Memory: 47.3 MB (5.19%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,31 +1,37 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
+        int rows=grid.length;
+        int cols=grid[0].length;
         Queue<int[]> queue=new LinkedList<>();
-        int max=0;
-        int[][] dirs={{1,0},{-1,0},{0,-1},{0,1}};
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1){
-                    int count=1;
-                    queue.add(new int[]{i,j});
-                    grid[i][j]=0;
-                    while(!queue.isEmpty()){
-                      int[] curr=queue.poll();
+        int[][] dirs={{0,1},{0,-1},{1,0},{-1,0}};
+        int max=Integer.MIN_VALUE; 
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==0) continue;
+                queue.add(new int[]{i,j});
+                grid[i][j]=0;
+                int count=0;
+                while(!queue.isEmpty()){
+                    count++;
+                    int[] curr=queue.poll();
+                    int r=curr[0];
+                    int c=curr[1];
 
-                      for(int[] k: dirs){
-                        int nr=curr[0]+k[0];
-                        int nc=curr[1]+k[1];
-                        if(nr>=0 && nc>=0 && nr<grid.length && nc<grid[0].length && grid[nr][nc]==1){
-                  count++;
-                  grid[nr][nc]=0;
-                  queue.add(new int[]{nr,nc});
+                    for(int[] dir: dirs){
+                        int nr=dir[0]+r;
+                        int nc=dir[1]+c;
+
+                        if(nr>=0 && nc>=0 && nr<rows && nc<cols && grid[nr][nc]==1){
+                            grid[nr][nc]=0;
+                            queue.add(new int[]{nr,nc});
                         }
-                      }
-                      max=Math.max(max,count);
                     }
                 }
+       
+                System.out.println(count);
+                max=Math.max(max,count);
             }
         }
-        return max;
+        return max==Integer.MIN_VALUE?max=0: max;
     }
 }
