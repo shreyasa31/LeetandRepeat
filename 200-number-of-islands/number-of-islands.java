3d9364352d62b91a08ae5fbbccde0de99b42a0b3
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-06-29 01:12:50 -0500

    Time: 8 ms (10.66%) | Memory: 51.5 MB (83.68%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,34 +1,39 @@
+
+
+import javax.management.QueryEval;
+
 class Solution {
     public int numIslands(char[][] grid) {
+        int rows=grid.length;int cols=grid[0].length;
         Queue<int[]> queue=new LinkedList<>();
-
-        int[][] dirs={{1,0},{-1,0},{0,-1},{0,1}};
+        int[][] dirs={{0,1},{0,-1},{1,0},{-1,0}};
         int count=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-               if(grid[i][j]=='1'){
-                  count++;
-                  queue.add(new int[]{i,j});
-                  grid[i][j]='0';
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+            if(grid[i][j]=='0') continue;
+            queue.add(new int[]{i,j});
+            grid[i][j]='0';
+            count++;
+            while(!queue.isEmpty()){
+            int[] curr=queue.poll();
+            int r=curr[0];
+            int c=curr[1];
+     
 
-                  while(!queue.isEmpty()){
-                    int[] curr=queue.poll();
-                     
-                    for(int[] k:dirs){
-                        int nr=curr[0]+k[0];
-                        int nc=curr[1]+k[1];
+            for(int[] dir:dirs){
+                int nr=r+dir[0];
+                int nc=c+dir[1];
 
-                        if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length &&grid[nr][nc]=='1'){
-                            grid[nr][nc]='0';
-                            queue.add(new int[]{nr,nc});
-                        }
-                    }
-                  }
-                  
-
-               }
+                if(nr>=0 && nc>=0 && nr<rows && nc<cols && grid[nr][nc]!='0'){
+                    grid[nr][nc]='0';
+                    queue.add(new int[]{nr,nc});
+                }
+            }
+        }
+     
             }
         }
         return count;
+
     }
 }
