597bb4d834a155ab243be4ca64055f8fe7cd8b88
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-06-29 18:07:08 -0500

    Time: 3 ms (13.56%) | Memory: 47.1 MB (57.30%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,33 +1,30 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
-        int[] color = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            color[i] = -1;
+        int n=graph.length;
+        int[] color= new int[n];
+        for(int i=0;i<n;i++){
+            color[i]=-1;
         }
-//here it can contains diconnected components thats why the old while queue empyty doesnt work
-        for (int i = 0; i < n; i++) {
-            if (color[i] == -1) {
-                Queue<Integer> queue = new LinkedList<>();
-                queue.add(i);
-                color[i] = 0;
+        Queue<Integer> queue=new LinkedList<>();
+      
+        for(int i=0;i<n;i++){
 
-                while (!queue.isEmpty()) {
-                    int curr = queue.poll();
+        queue.add(i);
+        color[0]=0;
+        while(!queue.isEmpty()){
+            int curr=queue.poll();
+            
 
-                    for (int k : graph[curr]) {
-                        if (color[k] == -1) {
-                            color[k] = 1 - color[curr];
-                            queue.add(k);
-                        } else if (color[k] == color[curr]) {
-                            return false;
-                        }
-                    }
+
+            for(int num:graph[curr]){
+                if(color[num]==-1){
+                    color[num]=1-color[curr];
+                    queue.add(num);
+                }else if(color[num] == color[curr]){
+                       return false;
                 }
             }
-        }
-
+        }}
         return true;
     }
 }
