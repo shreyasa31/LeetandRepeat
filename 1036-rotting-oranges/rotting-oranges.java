08311ac69f2304c4d937eaed3dbb82f4308ee17b
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-06-29 19:00:01 -0500

    Time: 2 ms (90.75%) | Memory: 44.3 MB (24.64%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,39 +1,40 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         int rows=grid.length;
-        int cols=grid[0].length; int fresh=0;
-        int[][] dirs={{1,0},{-1,0},{0,-1},{0,1}}; int max=0;
+        int cols=grid[0].length; int fresh=0; int max=0;
+           int[][] dirs={{1,0},{-1,0},{0,-1},{0,1}};
         Queue<int[]> queue=new LinkedList<>();
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(grid[i][j]==2){
-                
                     queue.add(new int[]{i,j,0});
                 }
-                if(grid[i][j]==1){
-                    fresh++;
-                }
+                if(grid[i][j]==1)
+                fresh++;
 
             }
         }
-        if(fresh==0) return 0;
-        while(!queue.isEmpty()){
-          int[] curr=queue.poll();
-          int r=curr[0];
-          int c=curr[1];
-          int mins=curr[2];
-          max=Math.max(max,mins);
-          for(int[] k: dirs){
-            int nr=k[0]+r;
-            int nc=k[1]+c;
-         if(nr>=0 && nc>=0 && nr<rows && nc<cols && grid[nr][nc]==1){
-            fresh--;
-            grid[nr][nc]=2;
         
-            queue.add(new int[]{nr,nc,mins+1});
-         }
-          }
+        while(!queue.isEmpty()){
+            int[] curr=queue.poll();
+
+            int r=curr[0];
+            int c=curr[1];
+             int t=curr[2];
+            max=Math.max(t,max);
+            for(int[] dir: dirs){
+                int nr=r+dir[0];
+                int nc=c+dir[1];
+
+                if(nr>=0 && nc>=0 && nr<rows && nc<cols && grid[nr][nc]==1){
+                    grid[nr][nc]='2';
+                    queue.add(new int[]{nr, nc,t+1});
+                    fresh--;
+                }
+            }
+
+
         }
-       return fresh>0?-1:max;
+        return fresh>0?-1:max;
     }
 }
