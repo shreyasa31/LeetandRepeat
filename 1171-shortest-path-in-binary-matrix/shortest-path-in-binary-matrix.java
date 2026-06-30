80ee4c1a59c8bcbcc071d28bd2e0bece375518e3
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-06-30 04:23:35 -0500

    Time: 13 ms (82.83%) | Memory: 47.9 MB (20.48%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,36 +1,29 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        Queue<int[]> queue=new LinkedList<>();
         int rows=grid.length;
         int cols=grid[0].length;
-        if(grid[0][0]==1) return -1;
-        if(grid[rows-1][cols-1]==1) return -1;
-        int[][] dir={{1,0},{-1,0},{0,-1},{0,1},{1,-1},{1,1},{-1,-1},{-1,1}};
-        int p=0;
-        boolean[][] visited=new boolean[rows][cols];
-            queue.add(new int[]{0,0,1});
-                visited[0][0]=true;
-                
-                while(!queue.isEmpty()){
-                    int [] curr=queue.poll();
-                    int r=curr[0];
-                    int c=curr[1];
-                    int d=curr[2];
-                    if(r==rows-1 &&  c==cols-1){
-                    return  d;
+        if(grid[0][0]==1 || grid[rows-1][cols-1]==1) return -1;
+        Queue<int[]> queue=new LinkedList<>();
+        queue.add(new int[]{0,0,1});
+        int[][] dirs={{1,0},{-1,0},{0,-1},{0,1},{1,1},{1,-1},{-1,-1},{-1,1}};
+        grid[0][0]=1;
+        while(!queue.isEmpty()){
+            int[] curr=queue.poll();
+            int r=curr[0];
+            int c=curr[1];
+            int d=curr[2];
+
+            if(r==rows-1 && c==cols-1) return d;
+
+            for(int[] dir:dirs){
+                int nr=r+dir[0];
+                int nc=c+dir[1];
+
+                if(nr>=0 && nc>=0 && nr<rows && nc<cols && grid[nr][nc]==0){
+                    grid[nr][nc]=1;
+                    queue.add(new int[]{nr,nc,d+1});
                 }
-                    for(int[] k:dir){
-                        int nr=k[0]+r;
-                        int nc=k[1]+c;
-
-                        if(nr>=0 && nc>=0 && nr<rows && nc<cols && grid[nr][nc]==0 && visited[nr][nc]==false)
-                        {
-                            visited[nr][nc]=true;
-                            queue.add(new int[]{nr,nc,d+1});
-
-                        }
-                    }
-            
+            }
         }
         return -1;
     }
