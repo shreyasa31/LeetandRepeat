52ca42e2607f26f671148ea6679719de6b6841e6
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-07-01 06:31:59 -0500

    Time: 75 ms (58.38%) | Memory: 48.6 MB (39.22%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -10,30 +10,30 @@ class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
         HashSet<String> set=new HashSet<>();
-        for(String i: wordList){
+        for(String i: wordList){ // N
             set.add(i);
         }
         
         Queue<Pair> queue=new LinkedList<>();
         queue.add(new Pair(beginWord,1));
 
-        while(!queue.isEmpty()){
+        while(!queue.isEmpty()){ //N
             Pair curr=queue.poll();
             String w=curr.word;
             int steps=curr.val;
             if(w.equals(endWord)){
                 return steps;
             }
-            for(int i=0;i<w.length();i++){
+            for(int i=0;i<w.length();i++){ //L
                 char[] arr=w.toCharArray();
 
-                for(char ch='a'; ch<='z';ch++){
+                for(char ch='a'; ch<='z';ch++){ //26
                     arr[i]=ch;
-                    String s=new String(arr);
+                    String s=new String(arr); // L
 
                     if(set.contains(s)){
-                        set.remove(s);
-                        queue.add(new Pair(s, steps+1));
+                        set.remove(s); //O(1)
+                        queue.add(new Pair(s, steps+1)); //O(1)
                     }
 
                 }
@@ -43,3 +43,5 @@ class Solution {
       return 0;
     }
 }
+
+//N*L*26*L= N*L square
