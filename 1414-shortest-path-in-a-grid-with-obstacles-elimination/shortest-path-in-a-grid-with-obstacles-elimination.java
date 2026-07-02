71bc4937c804206cbe486fb00b0221a0e09320b3
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-07-01 23:49:06 -0500

    Time: 15 ms (64.77%) | Memory: 46.3 MB (73.31%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -2,51 +2,42 @@ class Solution {
     public int shortestPath(int[][] grid, int k) {
         int rows=grid.length;
         int cols=grid[0].length;
+        int[][] dirs={{0,1},{0,-1},{1,0},{-1,0}};
         Queue<int[]> queue=new LinkedList<>();
-        int[][] dirs={{1,0},{-1,0},{0,-1},{0,1}};
-        queue.add(new int[]{0,0,k});
-
-        int[][] kdistance=new int[rows][cols];
-        for(int i=0;i<kdistance.length;i++){
-            for(int j=0;j<kdistance[0].length;j++){
-                kdistance[i][j]=-1;
+        queue.add(new int[]{0,0,k,0});
+        int[][] remainK=new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++)
+            {
+                remainK[i][j]=-1;
             }
         }
-        kdistance[0][0]=k;
-        int level=0;
+
+
+        remainK[0][0]=k;
         while(!queue.isEmpty()){
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                int[] curr=queue.poll();
-                int r=curr[0];
-                int c=curr[1];
-                int kd=curr[2];
-
-                if(r==rows-1 && c==cols-1){
-                    return level;
-                }
-
-                for(int[] l:dirs){
-              int nr=l[0]+r;
-              int nc=l[1]+c;
-
-              int rem=kd;
-
-              if(nr>=0 && nc>=0 && nr<rows && nc<cols && grid[nr][nc]==1){
-                rem--;
-              }
-
+            int[] curr=queue.poll();
+            int r=curr[0];
+            int c=curr[1];
+            int rk=curr[2];
+            int d=curr[3];
+            if(r==rows-1 && c==cols-1) return d;
+            for(int[] dir:dirs){
+                int nr=r+dir[0];
+                int nc=c+dir[1];
+                int rem=rk;
+              if (nr>=0 & nc>=0 && nr<rows && nc<cols){
 
               
-              if(nr>=0 && nc>=0 && nr<rows && nc<cols && kdistance[nr][nc]<rem){
-                   kdistance[nr][nc]=rem;
-                   queue.add(new int[]{nr,nc,rem});
-                  }
+                if(grid[nr][nc]==1)
+                    rem--;}
+                if(nr>=0 & nc>=0 && nr<rows && nc<cols && remainK[nr][nc]<rem){
+                    remainK[nr][nc]=rem;
+                    queue.add(new int[]{nr,nc,rem,d+1});
+                    
                 }
             }
-            level++;
         }
-
-      return -1;
+        return -1;
     }
 }
