ad12e654e59882daa150ebf85edcbf4bdb7e6094
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-07-01 22:04:21 -0500

    Time: 99 ms (41.37%) | Memory: 51.8 MB (25.43%) - LeetSync

--- 753-open-the-lock/open-the-lock.java
+++ 753-open-the-lock/open-the-lock.java
@@ -1,51 +1,36 @@
 class Solution {
     public int openLock(String[] deadends, String target) {
-        HashSet<String> deadendset=new HashSet<>(Arrays.asList(deadends));
-        if(deadendset.contains("0000")){
-            return -1;
+        HashSet<String> set=new HashSet<>();
+        for(String s: deadends){
+            set.add(s);
         }
-        
-        if(target.equals("0000")){
-            return 0;
-        }
+        Queue<Pair<String, Integer>> queue=new LinkedList<>();
+        if(set.contains("0000")) return -1;
+        if(target.equals("0000")) return 0;
+        if(set.contains(target)) return -1;
+        queue.add(new Pair<>("0000",0));
         HashSet<String> visited=new HashSet<>();
-
-
-        Queue<String> queue=new LinkedList<>();
-        queue.add("0000");
         visited.add("0000");
-        int steps=0;
         while(!queue.isEmpty()){
-             int size=queue.size();
-             for(int i=0;i<size;i++){
-                String curr=queue.poll();
-
-                if(curr.equals(target))
-                {
-                    return steps;
-                }
-
-                for(String s:nextstates(curr)){
-                    if(deadendset.contains(s)){
-                        continue;
-                    }
-                    if(!visited.contains(s)){
-                         visited.add(s);
-                         queue.add(s);
-                    }
-                }
-
-
-             }
-             steps++;
-           
-        }
+            Pair curr=queue.poll();
+            String currentNumber=curr.getKey().toString();
+            int value=(int)curr.getValue();
       
-     return -1;
+            if(currentNumber.equals(target)) return value;
+            for(String comb: nextString(currentNumber)){
+         
+               if(!visited.contains(comb) && !set.contains(comb)){
+                visited.add(comb);
+                queue.add(new Pair<>(comb, value+1));
+               }
+            }
+        }
+        return -1;
     }
-    List<String> nextstates(String s){
-       List<String> res=new ArrayList<>();
 
+    List<String> nextString(String s){
+
+       ArrayList<String> res=new ArrayList<>();
        for(int i=0;i<4;i++){
         char[] c= s.toCharArray();
         c[i]=(c[i]=='9')?'0':(char)(c[i]+1);
@@ -58,11 +43,6 @@ class Solution {
         c1[i]=(c1[i]=='0')?'9':(char)(c1[i]-1);
         res.add(new String(c1));
        }
-
-
-        return res;
+       return res;
     }
-
-
-
 }
