4e159b1716e33aebab6be8d2b477d6a77b570fee
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-07-03 07:36:28 -0500

    Time: 3 ms (98.62%) | Memory: 44 MB (72.53%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.java
+++ 269-alien-dictionary/alien-dictionary.java
@@ -1,61 +1,62 @@
 class Solution {
     public String alienOrder(String[] words) {
-        int[] indegree=new int[26];
-        boolean[] present=new boolean[26];
-        ArrayList<List<Integer>> list=new ArrayList<>();
-       for(int i=0;i<words.length;i++){
-        for(int j=0;j<words[i].length();j++){
-            present[words[i].charAt(j)-'a']=true;
+        List<List<Integer>> adjList=new ArrayList<>();
+        for(int i=0;i<26;i++)
+        {
+            adjList.add(new ArrayList<>());
         }
-       }
-        for(int i=0;i<26;i++){
-            list.add(new ArrayList<>());
-        }
-        for(int i=0;i<words.length-1;i++){
-           String first=words[i];
-           String second=words[i+1];
-           if(first.length()!=second.length() && first.startsWith(second)) return "";
-           int min=Math.min(first.length(), second.length());
-           for(int l=0;l<min;l++){
-               char c= first.charAt(l);
-               char c2=second.charAt(l);
 
-               if(c!=c2){
-                    // if (!list.get(c-'a').contains(c2-'a')) {
-                        list.get(c-'a').add(c2-'a');
-                        indegree[c2-'a']++;
-                    // }
-                     break;
-               }
-              
-           }
-           }
-           int c=0;
-           Queue<Integer> queue=new LinkedList<>();
-           for(int i=0;i<indegree.length;i++){
-               if(present[i] && indegree[i]==0){
-                queue.add(i);
-               }
-               if(present[i]){
-                c++;
-               }
-           }
-           int count=0;
-           StringBuilder s=new StringBuilder();
-           while(!queue.isEmpty()){
-            int curr=queue.poll();
-            count++;
-            s.append((char)(curr+'a'));
-            for(int k: list.get(curr)){
-                indegree[k]--;
-                if(indegree[k]==0){
-                    queue.add(k);
+        boolean[] present=new boolean[26];
+           for (String word : words) {
+            for (char ch : word.toCharArray()) {
+                present[ch - 'a'] = true;
+            }
+        }
+
+        int[] indegree=new int[26];
+            for(int i=0;i<words.length-1;i++){
+            String word1=words[i];
+            String word2=words[i+1];
+            if (word1.length() > word2.length() && word1.startsWith(word2)) {
+                return "";
+            }
+            int min=Math.min(word1.length(), word2.length());
+            for(int j=0;j<min;j++){
+                char c1=word1.charAt(j);
+                char c2=word2.charAt(j);
+               
+                
+                if(c1!=c2){
+                      adjList.get(c1-'a').add(c2-'a');
+                      indegree[c2-'a']++;
+                      break;
+
                 }
             }
-           }
+        }int count=0;
+        Queue<Integer> queue=new LinkedList<>();
+        for(int k=0;k<indegree.length;k++){
+            if(present[k]) count++;
+            if(present[k] && indegree[k]==0){
 
+                queue.add(k);
+            }
+        }
+        int currCount=0;
+       StringBuilder sb=new StringBuilder();
+        while(!queue.isEmpty()){
+            int curr=queue.poll();
 
-        if(count!=c) return "";
-        return s.toString();
+            sb.append((char)(curr+'a'));
+            currCount++;
+            for(int i:adjList.get(curr)){
+                indegree[i]--;
+                if(indegree[i]==0 && present[i]){
+                    queue.add(i);
+                }
+            }
+        }
+       if(currCount!=count) return "";
+       return sb.toString();
     }
 }
