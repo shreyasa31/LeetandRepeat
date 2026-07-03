b58f415bcfd2134c4faa78dd0d9d1dbdfd1a6b1a
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-07-03 08:24:29 -0500

    Time: 16 ms (90.36%) | Memory: 62.7 MB (86.37%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -1,53 +1,50 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        ArrayList<Integer> res=new ArrayList<>();
-        if (n == 1) {
-            res.add(0);
-            return res;
-        }
-        ArrayList<List<Integer>> list=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            list.add(new ArrayList<>());
-        }
-
+         List<Integer> res=new ArrayList<>();
+        if(n==1) return Arrays.asList(0);
         int[] degree=new int[n];
-        for(int[] k: edges){
-           int first=k[0];
-           int second=k[1];
-           list.get(first).add(second);
-           list.get(second).add(first);
-           degree[first]++;
-           degree[second]++;
+        
+        List<List<Integer>> adjList=new ArrayList<>();
+        for(int j=0;j<n;j++){
+            adjList.add(new ArrayList<>());
+        }
+        for(int[] edge:edges){
+           int f1=edge[0];
+           int f2=edge[1];
 
+           adjList.get(f1).add(f2);
+           adjList.get(f2).add(f1);
+
+           degree[f1]++;
+           degree[f2]++;
         }
         Queue<Integer> queue=new LinkedList<>();
-        for(int i = 0; i < n; i++){
-    if(degree[i] == 1){
-        queue.add(i);
-    }
-}
-       
-        int remnode=n;
-        while(remnode>2){
-        
-           int size=queue.size();
+        for(int i=0;i<degree.length;i++){
+             if(degree[i]==1){
+                queue.add(i);
+             }
+        }
+       int remNode=n;
+        while(remNode>2){
+            int size=queue.size();
+            remNode-=queue.size();
+         for(int l=0;l<size;l++){
 
-           for(int i=0;i<size;i++){
-
-            int curr=queue.poll();    
-            remnode--;
-            for(int k: list.get(curr)){
-                degree[k]--;
-                if(degree[k]==1){
-                    queue.add(k);
+            
+            int curr=queue.poll();
+         
+            for(int i: adjList.get(curr)){
+                degree[i]--;
+                if(degree[i]==1){
+                    queue.add(i);
                 }
             }
-
         }}
-       
-        while(!queue.isEmpty()){
-          res.add(queue.poll());
+
+    
+        for(int k=0;k<remNode;k++){
+            res.add(queue.poll());
         }
-       return res;
+        return res;
     }
 }
