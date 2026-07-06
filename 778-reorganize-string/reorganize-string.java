9bdf5d1307b34da580e552b5afeebae47f5a37cf
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-07-06 03:29:58 -0500

    Time: 10 ms (5.73%) | Memory: 43.1 MB (54.35%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -2,37 +2,43 @@ class Solution {
     public String reorganizeString(String s) {
         HashMap<Character, Integer> map=new HashMap<>();
         for(char c:s.toCharArray()){
-            map.put(c, map.getOrDefault(c, 0)+1);
+            map.put(c, map.getOrDefault(c,0)+1);
         }
 
         PriorityQueue<Character> maxHeap=new PriorityQueue<>((a,b)-> map.get(b)-map.get(a));
-        StringBuilder sb=new StringBuilder();
         for(char c:map.keySet()){
             maxHeap.add(c);
         }
+
+
+
+        StringBuilder sb=new StringBuilder();
         while(maxHeap.size()>1){
             char first=maxHeap.poll();
-            char sec=maxHeap.poll();
+            char second=maxHeap.poll();
+
             sb.append(first);
-            sb.append(sec);
+            sb.append(second);
 
             map.put(first, map.get(first)-1);
-             map.put(sec, map.get(sec)-1);
+            map.put(second, map.get(second)-1);
 
-             if(map.get(first)>0){
+
+            if(map.get(first)>0){
                 maxHeap.add(first);
-             }
+            }
 
-              if(map.get(sec)>0){
-                maxHeap.add(sec);
-             }
-
-
-
+            if(map.get(second)>0){
+                maxHeap.add(second);
+            }
         }
+
         while(!maxHeap.isEmpty()){
-            if(map.get(maxHeap.peek())==1){
-                sb.append(maxHeap.poll());
+            char f=maxHeap.poll();
+            System.out.println(f +""+map.get(f));
+            if(map.get(f)==1)
+            {
+                sb.append(f);
             }else{
                 return "";
             }
