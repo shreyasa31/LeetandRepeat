cb66441e3791e1cc036276383d8d9df194857567
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-07-06 03:56:13 -0500

    Time: 1 ms (100.00%) | Memory: 46.3 MB (60.76%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,22 +1,25 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int[] changes = new int[1001]; // Based on problem constraint (locations <= 1000)
+        int[] arr=new int[1001];
 
-        for (int[] trip : trips) {
-            int num = trip[0];
-            int from = trip[1];
-            int to = trip[2];
 
-            changes[from] += num;   // Pick up
-            changes[to] -= num;     // Drop off
+        int curr=0;
+        for(int[] trip:trips){
+            int p=trip[0];
+            int from=trip[1];
+            int to=trip[2];
+
+            arr[from]+=p;
+            arr[to]-=p;
+
+
         }
 
-        int current = 0;
-        for (int i = 0; i < changes.length; i++) {
-            current += changes[i];
-            if (current > capacity) return false;
-        }
+        for(int i=0;i<arr.length;i++){
+            curr+=arr[i];
 
+            if(curr>capacity) return false;
+        }
         return true;
     }
 }
