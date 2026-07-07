2415f4c9f0e28ef4798b0d7da0684819090b85d7
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-07-07 17:51:25 -0500

    Time: 57 ms (21.12%) | Memory: 69.5 MB (81.62%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,32 +1,29 @@
-import java.util.HashMap;
-
 class Solution {
     public int totalFruit(int[] fruits) {
-        int n = fruits.length;
-        int i = 0, j = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int pick = 0;
-        int max = 0;
+        HashMap<Integer, Integer> map=new HashMap<>();
+        int i=0;
+        int j=0; int count=0; int max1=0;
+        while(j<fruits.length){
+            map.put(fruits[j], map.getOrDefault(fruits[j], 0)+1);
+            if(map.size()<=2){
+               
+               max1=Math.max(max1, j-i+1);
+               j++;
+            }else{
 
-        while (j < n) {
-            int r = fruits[j];
-
-            if (map.size() < 2 || map.containsKey(r)) {
-                // safe to add right fruit
-                map.put(r, map.getOrDefault(r, 0) + 1);
-                pick++;
-                if (pick > max) max = pick;
-                j++; // move right only after a valid add
-            } else {
-                // need to make room by removing one from the left
-                int l = fruits[i];
-                int c = map.get(l) - 1;
-                if (c == 0) map.remove(l);
-                else map.put(l, c);
-                pick--;
-                i++; // move left
+   
+      
+              if(map.containsKey(fruits[i])){
+                map.put(fruits[i], map.get(fruits[i])-1);
+                if(map.get(fruits[i])==0){
+                    map.remove(fruits[i]);
+                }
+              }
+              i++;j++;
+           
             }
         }
-        return max;
+    
+       return max1;
     }
 }
