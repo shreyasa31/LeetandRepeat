a36246bca8fb58818d8659d768c93c253e812fc6
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-07-07 18:12:01 -0500

    Time: 41 ms (11.09%) | Memory: 47.6 MB (7.57%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,27 +1,27 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         HashMap<Character, Integer> map=new HashMap<>();
-
-        int i=0;int j=0; int res=Integer.MIN_VALUE; int max=0;
+        int maxfreq=0;
+        int i=0;int j=0; int m=0;
         while(j<s.length()){
+            char c=s.charAt(j);
+            map.put(c, map.getOrDefault(c, 0)+1);
+            maxfreq=Math.max(maxfreq, map.get(c));
 
-           char c=s.charAt(j);
-           map.put(c,map.getOrDefault(c,0)+1);
-           max= Math.max(max, map.get(c));
+            if((j-i+1)-maxfreq<=k){
+                m=Math.max(m, j-i+1);
+                j++;
+                 
 
-           if((j-i+1)-max>k){
+            }else{
                
-                 map.put(s.charAt(i),map.get(s.charAt(i))-1);
-                    if (map.get(s.charAt(i)) == 0) {
+                map.put(s.charAt(i), map.get(s.charAt(i))-1);
+                if(map.get(s.charAt(i))==0){
                     map.remove(s.charAt(i));
                 }
-                i++;
-               
-           }
-           res=Math.max(res,j-i+1);
-           j++;
-
+                i++;j++;
+            }
         }
-return res;
+        return m;
     }
 }
