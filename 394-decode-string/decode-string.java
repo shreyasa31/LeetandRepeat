e5b7a18bd47e32d8c6766815efee48715a956c68
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-07-08 18:32:22 -0500

    Time: 1 ms (86.00%) | Memory: 43 MB (40.50%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,29 +1,30 @@
 class Solution {
     public String decodeString(String s) {
         Stack<Integer> count=new Stack<>();
-        Stack<StringBuilder> stringStack=new Stack<>();
-        StringBuilder currentString=new StringBuilder();
-        int k=0;
-        for(char c:s.toCharArray()){
-            if(Character.isDigit(c)){
-                k=k*10+(c-'0');
-             }else if(c=='['){
-                count.push(k);
-                stringStack.push(currentString);
-                currentString=new StringBuilder();
-                k=0;
-             }else if(c==']'){
-                int repeat=count.pop();
-                StringBuilder d=stringStack.pop();
-                for(int i=0;i<repeat;i++){
-                    d.append(currentString);
-                }
-                currentString=d;
-             }else{
-                currentString.append(c);
-             }
-        }
+        Stack<StringBuilder> stringb=new Stack<>();
+        StringBuilder sb=new StringBuilder(); int k=0;
+        for(int i=0;i<s.length();i++){
+          char c=s.charAt(i);
 
-        return currentString.toString();
+          if(Character.isDigit(c)){
+            k=k*10 +(c-'0');
+          }else if(c=='['){
+            count.push(k);
+            stringb.push(sb);
+
+            k=0;
+            sb=new StringBuilder();
+          }else if(c==']'){
+             int repeat=count.pop();
+             StringBuilder letter=stringb.pop();
+             for(int j=0;j<repeat;j++){
+                  letter.append(sb);
+             }
+             sb=letter;
+          }else{
+            sb.append(c);
+          }
+        }
+        return sb.toString();
     }
 }
