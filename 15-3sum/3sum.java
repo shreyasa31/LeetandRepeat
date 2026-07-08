5c17e67a3a92f99b0154314aa84d76bdd1892484
Shreya Sadashiva Kamath <143358049+shreyasa31@users.noreply.github.com>
2026-07-08 01:58:58 -0500

    Time: 33 ms (73.90%) | Memory: 60.5 MB (7.73%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,37 +1,48 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-     Arrays.sort(nums);
-     ArrayList<List<Integer>> res=new ArrayList<>();
-     for(int i=0;i<nums.length-2;i++){
-        int j=i+1;
-        int k=nums.length-1;
-        while(i<nums.length-1 && nums[i]==nums[i+1]){
-            i++;
-        }
-        while(j<k){
-            int sum=nums[i]+nums[j]+nums[k];
-            if(sum>0){
-                k--;
-
-            }else if(sum<0){
-                j++;
-            }else{
-                res.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                j++;
-                k--;
-
-
-
-                while(j<k && nums[j]==nums[j-1]){
-                   j++;
-                }
-
-                while(j<k && nums[k]==nums[k+1]){
+        Arrays.sort(nums);
+        int i=0; int sum=0;
+        List<List<Integer>> res=new ArrayList<>();
+        while(i<nums.length-2){
+            int j=i+1;
+            int k=nums.length-1;
+            while(i<nums.length-1 && nums[i]==nums[i+1]){
+                    i++;
+                   }
+            while(j<k){
+                sum=nums[i]+nums[j]+nums[k];
+                if(sum>0){
                     k--;
+                }else if(sum<0){
+                  j++;
+                }else{
+                    ArrayList<Integer> list=new ArrayList<>();
+                    list.add(nums[i]);list.add(nums[j]);list.add(nums[k]);
+                   res.add(list);
+                   j++;
+                   k--;
+
+
+
+                   while(j<k && nums[j]==nums[j-1]){
+                    j++;
+                   }
+                   while(j<k && nums[k]==nums[k+1]){
+                    k--;
+                   }
                 }
             }
+            i++;
+           
+
+
+
+
+
+
+
+
         }
-     }
-     return res;
+        return res;
     }
 }
